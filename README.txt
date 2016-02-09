e82838cf8a222d2fbb07882593545da0b5bb555e
tmmluis <tmmluis@gmail.com>
2016-02-09 18:17:03 +0200

    Moved all the Wikimedia API related methods into the Wikipedia object & add README

---
+++ README.txt
@@ -0,0 +1,8 @@
+A web app that is basically a Wikipedia search engine.
+Search Wikipedia articles by keyword or load arandom article.
+App built using AngularJS and the MediaWiki API.
+
+Try it out here: http://codepen.io/tmmluis/full/RrZdGW/
+
+Feel free to share or modify.
+@2016 Tiago Luis
--- scripts/wikiviewer.js
+++ scripts/wikiviewer.js
@@ -4,135 +4,119 @@
   var app = angular.module('wikipediaViewer', []);
 
   /*
-  * Creating and registering a controller with our module.
-  * Adding the '$http' service as a dependency so we can get
-  * JSON from Wikipedia api.
-  * Adding the'$sce' service so we can explicitly trust html returned
-  * from Wikipedia and use 'ng-bind-html'.
+  * The app controller and its dependencies:
+  *   '$http' to request JSON from the server.
+  *   '$sce' to bind returned HTML content via 'ng-bind-html'.
+  *   '$q' to intercept the asynchronous requests.
   */
   app.controller('SearchController', [ '$scope', '$http', '$sce', '$q',
       function ($scope, $http, $sce, $q) {
-    // Initialization
-    $scope.articles = [];
-    $scope.keyword = '';
-    var ENDPOINT = 'https://en.wikipedia.org/w/api.php?';
 
-    // Triggered when 'search' is pressed.
     $scope.search = function(keyword) {
-      // Reset previous results.
-      $scope.articles = [];
+      Wikipedia.search(keyword).then(function(articles) {
+        $scope.articles = articles;
+      }).catch(function(error){
+        console.log("There was an error querying Wikipedia: " + error);
+      });
+    };
 
+    $scope.randomArticle = function() {
+      Wikipedia.randomArticle().then(function(article) {
+        $scope.articles = article;
+      }).catch(function(error){
+        console.log("There was an error querying Wikipedia: " + error);
+      });
+    };
+
+    var Wikipedia = {
       /*
-      * Building the search query URL.
+      * Building the Wikimedia API request URL.
       * To avoid cross-domain restrictions we'll be using the 'jsonp'
       * method with 'callback=JSON_CALLBACK'.
       */
-      var query = 'action=query&list=search&srsearch=';
-      var parameters = '&format=json&callback=JSON_CALLBACK';
+      buildRequestURL: function(action, criteria) {
+        var endpoint = 'https://en.wikipedia.org/w/api.php?';
+        var parameters = '';
+        var criteria = criteria || '';
+        var format = '&format=json&callback=JSON_CALLBACK';
 
-      var url = ENDPOINT + query + keyword + parameters;
-
-      // Perform the JSON request, parse the result, and chain a second
-      // request to get more info about the returned pages.
-      Wikipedia.search(url).then(function(infoURL) {
-        Wikipedia.getInfo(infoURL).then(function(info) {
-          parseInfo(info);
-        });
-      })
-        .catch(function(error){
-          console.log("There was an error querying Wikipedia: " + error);
-      });
-    };
-
-    $scope.randomArticle = function(){
-      // Reset previous results.
-      $scope.articles = [];
-
-      var query = 'action=query&list=random&rnnamespace=0';
-      var parameters = '&format=json&callback=JSON_CALLBACK';
-
-      var url = ENDPOINT + query + parameters;
-
-      $http.jsonp(url)
-        .then(function(response){
-          var page = response.data.query.random[0];
-          var article = {
-            pageid: page.id,
-            title: page.title
-          };
-          query = 'https://en.wikipedia.org/w/api.php?'
-            + 'action=query&prop=extracts|info&format=json&explaintext=&exchars=300&inprop=url&pageids='
-            + article.pageid
-            + '&callback=JSON_CALLBACK';
-          $scope.articles.push(article);
-          return $http.jsonp(query);
-        })
-        .then(function(response){
-          var article =  $scope.articles[0];
-          var page = response.data.query.pages[article.pageid];
-          article.snippet = $sce.trustAsHtml('<p>' + page.extract + '</p>');
-          article.url = page.fullurl;
-        })
-        .catch(function(error){
-          $scope.error = "An error has occured!";
-          console.log(error);
-      });
-    };
-
-    /*  Parses an Array of search results and builds the 'articles' object.
-    *   returns the url request necessary to get additional info for each
-    *   of the pages.
-    */
-    function parseSearch(searchResults){
-      var titles = '';
-      for (var i = 0; i < searchResults.length; i++) {
-        $scope.articles.push({
-          title: searchResults[i].title,
-          snippet: $sce.trustAsHtml(searchResults[i].snippet + ' (...)')
-        });
-        titles += searchResults[i].title;
-        if (i < searchResults.length - 1) {
-          titles += "|";
-        }
-      }
-      /* Building info query URL. This will provide us more
-      * information about each article.
-      */
-      var query = 'action=query&prop=info&inprop=url&titles=';
-      var parameters = '&format=json&callback=JSON_CALLBACK';
-      return ENDPOINT + query + titles + parameters;
-    }
-
-    /*
-    *   Parses an Array of pages (obtained from querying Wikipedia for info)
-    *   and updates the 'articles' object with that data.
-    */
-    function parseInfo(pages) {
-      for (article in pages) {
-        for (var j = 0; j < $scope.articles.length; j++) {
-          if (pages[article].title === $scope.articles[j].title) {
-            $scope.articles[j].url = pages[article].fullurl;
-            $scope.articles[j].pageid = pages[article].pageid;
+        switch(action) {
+          case 'search':
+            parameters = 'action=query&list=search&srsearch=';
             break;
-          }
+          case 'infoFromTitles':
+            parameters = 'action=query&prop=info&inprop=url&titles=';
+            break;
+          case 'random':
+            parameters = 'action=query&list=random&rnnamespace=0';
+            break;
+          case 'infoFromId':
+            parameters = 'action=query&prop=extracts|info&explaintext=&exchars=300&inprop=url&pageids=';
+            break;
         }
-      }
-    }
-
-    var Wikipedia = {
-      search: function(request) {
+        var query = parameters + criteria;
+        return endpoint + query + format;
+      },
+      // Returns an Array of Wikipedia articles matching to the keyword.
+      search: function(keyword) {
+        var url = Wikipedia.buildRequestURL('search', keyword);
         var deferred = $q.defer();
-        $http.jsonp(request).then(function(response) {
-          var results = response.data.query.search;
-          var infoURL = parseSearch(results);
-          deferred.resolve(infoURL);
+        var articles = [];
+
+        $http.jsonp(url).then(function(response) {
+          articles = response.data.query.search;
+          var titles = '';
+
+          for (var i = 0; i < articles.length; i++) {
+            // Parse the article snippet as trusted HTML so it can display properly.
+            articles[i].snippet = $sce.trustAsHtml(articles[i].snippet + ' (...)');
+            // Concatenate all article titles into one String object.
+            titles += articles[i].title;
+            if (i < articles.length - 1) {
+              titles += "|";
+            }
+          }
+          var infoURL = Wikipedia.buildRequestURL('infoFromTitles', titles);
+          // Query Wikipedia for additional information on the provided titles.
+          // This will provide us URL's for each article.
+          $http.jsonp(infoURL).then(function(response) {
+            var pages = response.data.query.pages;
+            for (page in pages) {
+              for (var i = 0; i < articles.length; i++) {
+                if (pages[page].title === articles[i].title) {
+                  articles[i].url = pages[page].fullurl;
+                  articles[i].id = pages[page].pageid;
+                  break;
+                }
+              }
+            }
+            deferred.resolve(articles);
+          });
         });
         return deferred.promise;
       },
-      getInfo: function(request) {
+      /*
+      *   Processes a request to Wikipedia for a random page and returns an
+      *   Array with a single 'article' object.
+      */
+      randomArticle: function(request) {
+        var url = Wikipedia.buildRequestURL('random');
         var deferred = $q.defer();
-        $http.jsonp(request).then(function(response) {
-          deferred.resolve(response.data.query.pages);
+
+        $http.jsonp(url).then(function(response) {
+          var article = response.data.query.random[0];
+          var infoURL = Wikipedia.buildRequestURL('infoFromId', article.id);
+
+          $http.jsonp(infoURL).then(function(response) {
+            var articleInfo = response.data.query.pages[article.id];
+            var output = [];
+
+            article.snippet = $sce.trustAsHtml('<p>' + articleInfo.extract + '</p>');
+            article.url = articleInfo.fullurl;
+
+            output.push(article);
+            deferred.resolve(output);
+          });
         });
         return deferred.promise;
       }
